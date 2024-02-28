bb0ad5dd0a3ef11e7167457ba7bac9c410120130
sebs2837 <66304684+sebs2837@users.noreply.github.com>
2024-02-28 07:42:39 +0100

    fix: removed cache file

---
+++ nvim/lua/sebs/plugins/linter.lua
@@ -0,0 +1,18 @@
+local config = function()
+    require('lint').linters_by_ft = {
+        markdown = {'markdownlint',}
+    }
+    vim.api.nvim_create_autocmd({'BufWritePost'}, {
+        group = vim.api.nvim_create_augroup('SebsLinters', {}),
+        callback = function (ev)
+            require('lint').try_lint()
+        end,
+    })
+end
+
+return {
+    {
+        "mfussenegger/nvim-lint",
+        config = config
+    }
+}
